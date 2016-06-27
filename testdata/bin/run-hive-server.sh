3364db6ec6a78bdc0e20c6c7db79a379255d19a1
segelyang <zhizhiy@cn.ibm.com>
2016-06-27 17:35:34 +0800

    Fix hiveserver outofmemory issue

--- testdata/bin/run-hive-server.sh
+++ testdata/bin/run-hive-server.sh
@@ -47,7 +47,7 @@ done
 ${CLUSTER_BIN}/kill-hive-server.sh &> /dev/null
 
 # Starts a Hive Metastore Server on the specified port.
-HADOOP_CLIENT_OPTS=-Xmx2024m hive --service metastore -p $HIVE_METASTORE_PORT \
+HADOOP_CLIENT_OPTS="-Xmx2024m -XX:MaxPermSize=512M" hive --service metastore -p $HIVE_METASTORE_PORT \
     > ${LOGDIR}/hive-metastore.out 2>&1 &
 
 # Wait for the Metastore to come up because HiveServer2 relies on it being live.
@@ -56,7 +56,7 @@ ${CLUSTER_BIN}/wait-for-metastore.py --transport=${METASTORE_TRANSPORT}
 if [ ${ONLY_METASTORE} -eq 0 ]; then
   # Starts a HiveServer2 instance on the port specified by the HIVE_SERVER2_THRIFT_PORT
   # environment variable.
-  hive --service hiveserver2 > ${LOGDIR}/hive-server2.out 2>&1 &
+  HADOOP_CLIENT_OPTS="-XX:MaxPermSize=512M" hive --service hiveserver2 > ${LOGDIR}/hive-server2.out 2>&1 &
 
   # Wait for the HiveServer2 service to come up because callers of this script
   # may rely on it being available.
